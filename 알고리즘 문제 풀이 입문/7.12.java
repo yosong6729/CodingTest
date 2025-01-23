07bc66bffd32a782317f563bbedaddeb65b34d90
byeunghoon Kang <qudgns119@naver.com>
2025-01-23 14:46:08 +0900

    코테 풀이

---
+++ 알고리즘 문제 풀이 입문/7.12.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class Main {
+
+	static int n;
+	static int m;
+	static int answer;
+	static int[][] graph;
+	static int[] ch;
+	public void DFS(int v) {
+		if(v == n) {
+			answer++;
+			return;
+		}else {
+			for(int i = 1; i <= n; i++) {
+				if(graph[v][i] == 1 && ch[i] == 0) {
+					ch[i] = 1;
+					DFS(i);
+					ch[i] = 0;
+				}
+			}
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		Main T = new Main();	
+		Scanner kb = new Scanner(System.in);
+		
+		n = kb.nextInt();
+		m = kb.nextInt();
+		graph = new int[n + 1][n + 1];
+		
+		for(int i = 0; i < m; i ++) {
+			int a = kb.nextInt();
+			int b = kb.nextInt();
+			graph[a][b] = 1;
+		}
+		ch = new int[n + 1];
+		ch[1] = 1;
+		
+		T.DFS(1);
+		System.out.print(answer);
+	}
+}
