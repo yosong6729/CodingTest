2a708fe643cd48f172d56559342ae7adef9e9070
byeunghoon Kang <qudgns119@naver.com>
2025-02-23 21:38:48 +0900

    코테 풀이

---
+++ 알고리즘 문제 풀이 입문/1972.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Main {
+	
+	public static void main(String[] args){
+		Main T = new Main();
+		Scanner kb = new Scanner(System.in);
+		
+		int n = kb.nextInt();
+		PriorityQueue<Integer> PQ = new PriorityQueue<>();
+		ArrayList<Integer> result = new ArrayList<>();
+		
+		
+		for(int i = 0; i < n; i++) {
+			int x = kb.nextInt();
+			
+			if(x != 0) {
+				PQ.offer(x);
+			}
+			
+			if(x == 0) {
+				if(PQ.size() == 0) {
+					result.add(0);
+				}else {
+					result.add(PQ.poll());
+				}
+			}
+		}
+		
+		for(int x : result) {
+			System.out.println(x);
+		}
+	}
+}
