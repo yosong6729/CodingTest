2a48471766ca77494728bbadfc1e3c834d9d0d40
byeonghoon <qudgns119@naver.com>
2025-06-11 15:28:51 +0900

    코테 풀이

--- 알고리즘 문제 풀이 입문/1-7-2.java
+++ 알고리즘 문제 풀이 입문/1-7-2.java
@@ -1,55 +1,8 @@
 import java.util.*;
 import java.io.*;
 
-class Point{
-	int x;
-	int y;
-	
-	Point(int x, int y){
-		this.x = x;
-		this.y = y;
-	}
-}
-
 public class Main {
-	
-	static int[] dx = {-1, -1, 0, 1, 1, 1, 0, -1};
-	static int[] dy = {0, 1, 1, 1, 0, -1, -1, -1};
-	
-	public static int BFS(int x, int y, int n, int[][] keypad) {
-		Queue<Point> Q = new LinkedList<>();
-		Q.offer(new Point(x, y));
-		int L = 0;
-		boolean[][] visited = new boolean[3][3];
-		visited[x][y] = true;
-		
-		while(!Q.isEmpty()){
-			
-			int len = Q.size();
-			for(int i = 0; i < len; i++) {
-				Point p = Q.poll();
-				int px = p.x;
-				int py = p.y;
-				if(keypad[px][py] == n) {
-					return L;
-				}
-				
-				for(int j = 0; j < 8; j++) {
-					int nx = px + dx[j];
-					int ny = py + dy[j];
-					
-					if(nx >= 0 && nx < 3 && ny >= 0 && ny < 3
-							&& !visited[nx][ny]) {
-						visited[nx][ny] = true;
-						Q.offer(new Point(nx, ny));
-					}
-					
-				}
-			}
-			L++;
-		}
-		return 0;
-	}
+
 	
 	public int solution(int[] keypad, String password){
 		int answer = 0;
@@ -60,15 +13,41 @@ public class Main {
 			newKeypad[i / 3][i % 3] = keypad[i];
 		}
 		
+		int[][] dist = new int[10][10];
 		
-		for(int i = 0; i < password.length() - 1; i++) {
+		
+		for(int i = 0; i < 10; i++) {
+			Arrays.fill(dist[i], 2);
+		}
+		
+		for(int i = 0; i < 10; i++) {
+			dist[i][i] = 0;
+		}
+		
+		int[] dx = {-1, -1, 0, 1, 1, 1, 0, -1};
+		int[] dy = {0, 1, 1, 1, 0, -1, -1, -1};
+		
+		
+		for(int i = 0; i < 3; i++) {
 			for(int j = 0; j < 3; j++) {
-				for(int k = 0; k < 3; k++) {
-					if(newKeypad[j][k] == (password.charAt(i) - '0')) {
-						answer += BFS(j, k, password.charAt(i + 1) - '0', newKeypad);
+				int from = newKeypad[i][j];
+				for(int k = 0; k < 8; k++) {
+					int nx = i + dx[k];
+					int ny = j + dy[k];
+					
+					if(nx >= 0 && nx < 3 && ny >= 0 && ny < 3) {
+						int to = newKeypad[nx][ny];
+						dist[from][to] = 1;
 					}
 				}
 			}
+		}
+		
+		for(int i = 0; i < password.length() - 1; i++) {
+			int from = password.charAt(i) - '0';
+			int to = password.charAt(i + 1) - '0';
+			
+			answer += dist[from][to];
 		}
 		
 		return answer;
